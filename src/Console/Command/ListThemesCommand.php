07c9c5cbdf78d8373f62de135b2b81ad824dd356
Mathias Elle <m.elle@e3n.de>
2024-12-13 08:29:39 +0100

    Refactor ListThemesCommand: remove unused constructor and improve code clarity

--- src/Console/Command/ListThemesCommand.php
+++ src/Console/Command/ListThemesCommand.php
@@ -12,35 +12,21 @@ use Magento\Framework\Console\Cli;
 
 class ListThemesCommand extends Command
 {
-    /**
-     * Constructor
-     *
-     * @param ThemeList $themeList
-     */
     public function __construct(
         private readonly ThemeList $themeList,
     ) {
         parent::__construct();
     }
 
-    /**
-     * Configure the command
-     */
-    protected function configure(): void {
+    protected function configure(): void
+    {
         $this->setName('mageforge:themes:list');
         $this->setDescription('Lists all available themes');
     }
 
-    /**
-     * Execute the command
-     *
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @return int
-     */
     protected function execute(
         InputInterface $input,
-        OutputInterface $output
+        OutputInterface $output,
     ): int {
         $themes = $this->themeList->getAllThemes();
 
