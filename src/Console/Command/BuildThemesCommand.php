0644ccd244c17b8bdf9c13ed482047033797fa56
Mathias Elle <m.elle@e3n.de>
2024-12-20 23:30:21 +0100

    Enhance BuildThemesCommand with improved file checks and execution flow, including timing and success messages for theme builds

--- src/Console/Command/BuildThemesCommand.php
+++ src/Console/Command/BuildThemesCommand.php
@@ -46,114 +46,136 @@ class BuildThemesCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $themeCodes = $input->getArgument('themeCodes');
         $themesCount = count($themeCodes);
+        $startTime = microtime(true);
 
-        # $io->confirm(question: "Build all " . $themesCount . " Themes?", false);
-
-        $io->title(count($themeCodes) > 1
+        $io->title($themesCount > 1
             ? 'Build ' . $themesCount . ' themes! This can take a while, please wait.'
-            : 'Build the theme.');
+            : 'Build the theme. Please wait...');
+
         foreach ($themeCodes as $themeCode) {
             $themePath = $this->themePath->getPath($themeCode);
             if ($themePath === null) {
                 $io->error("Theme $themeCode is not installed.");
                 continue;
             }
-            $io->section("Theme Code: $themeCode");
+            $io->section("Theme: $themeCode");
 
-            # Check package.json file
-            if (!file_exists('package.json')) {
-                $io->warning("The 'package.json' file does not exist in the Magento root path.");
-                if (!file_exists('package.json.sample')) {
-                    $io->warning("The 'package.json.sample' file does not exist in the Magento root path.");
-                    $io->error("Skip this theme build.");
-                    continue;
-                } else {
-                    $io->success("The 'package.json.sample' file found.");
-                    if ($io->confirm("Do you want to copy 'package.json.sample' to 'package.json'?", false)) {
-                        copy('package.json.sample', 'package.json');
-                        $io->success("'package.json.sample' has been copied to 'package.json'.");
-                    }
-                }
-            } else {
-                $io->success("The 'package.json' file found.");
-            }
-
-            # Check node_modules folder
-            if (!is_dir('node_modules')) {
-                $io->warning("The 'node_modules' folder does not exist in the Magento root path.");
-                if ($io->confirm("Do you want to run 'npm install' to install the dependencies? ", false)) {
-                    $io->section("Running 'npm install'... This can take a while, please wait.");
-                    exec('npm install', $outputLines, $resultCode);
-                    if ($resultCode === 0) {
-                        $io->success("'npm install' has been successfully executed.");
-                    } else {
-                        $io->error("'npm install' failed. Please check the output for more details.");
-                        continue;
-                    }
-                } else {
-                    $io->error("Skip this theme build.");
-                    continue;
-                }
-            } else {
-                $io->success("The 'node_modules' folder found.");
-            }
-
-            # Check Gruntfile.js file
-            if (!file_exists('Gruntfile.js')) {
-                $io->warning("The 'Gruntfile.js' file does not exist in the Magento root path.");
-                if (!file_exists('Gruntfile.js.sample')) {
-                    $io->warning("The 'Gruntfile.js.sample' file does not exist in the Magento root path.");
-                    $io->error("Skip this theme build.");
-                    continue;
-                } else {
-                    $io->success("The 'Gruntfile.js.sample' file found.");
-                    if ($io->confirm("Do you want to copy 'Gruntfile.js.sample' to 'Gruntfile.js'?", false)) {
-                        copy('Gruntfile.js.sample', 'Gruntfile.js');
-                        $io->success("'Gruntfile.js.sample' has been copied to 'Gruntfile.js'.");
-                    }
-                }
-            } else {
-                $io->success("The 'Gruntfile.js' file found.");
-            }
-
-            # check grunt-config.json
-            // if (!file_exists('grunt-config.json')) {
-            //     $io->warning("The 'grunt-config.json' file does not exist in the Magento root path.");
-            //     if (!file_exists('grunt-config.json.sample')) {
-            //         $io->warning("The 'grunt-config.json.sample' file does not exist in the Magento root path.");
-            //         $io->error("Skip this theme build.");
-            //         continue;
-            //     } else {
-            //         $io->success("The 'grunt-config.json.sample' file found.");
-            //         if ($io->confirm("Do you want to copy 'grunt-config.json.sample' to 'grunt-config.json'?", false)) {
-            //             copy('grunt-config.json.sample', 'grunt-config.json');
-            //             $io->success("'grunt-config.json.sample' has been copied to 'grunt-config.json'.");
-            //         }
-            //     }
-            // } else {
-            //     $io->success("The 'grunt-config.json' file found.");
-            // }
-
-            # Run Grunt
-            $io->section("Running 'grunt'... This can take a while, please wait.");
-            exec('node_modules/.bin/grunt clean', $outputLines, $resultCode);
-            $io->writeln($outputLines);
-            if ($resultCode === 0) {
-                $io->success("'grunt clean' has been successfully executed.");
-            } else {
-                $io->error("'grunt clean' failed. Please check the output for more details.");
-                continue;
-            }
-            exec('node_modules/.bin/grunt less', $outputLines, $resultCode);
-            $io->writeln($outputLines);
-            if ($resultCode === 0) {
-                $io->success("'grunt less' has been successfully executed.");
-            } else {
-                $io->error("'grunt less' failed. Please check the output for more details.");
+            if (!$this->checkPackageJson($io) || !$this->checkNodeModules($io)) {
                 continue;
             }
 
+            // Check Gruntfile.js file
+            if (!$this->checkFile($io, 'Gruntfile.js', 'Gruntfile.js.sample')) {
+                continue;
+            }
+
+            // Run Grunt only on the first run
+            static $isFirstRun = true;
+            if ($isFirstRun) {
+                $io->section("Running 'grunt'... Please wait.");
+                exec('node_modules/.bin/grunt clean', $outputLines, $resultCode);
+                $io->writeln($outputLines);
+                if ($resultCode === 0) {
+                    $io->success("'grunt clean' has been successfully executed.");
+                } else {
+                    $io->error("'grunt clean' failed. Please check the output for more details.");
+                    continue;
+                }
+                exec('node_modules/.bin/grunt less', $outputLines, $resultCode);
+                $io->writeln($outputLines);
+                if ($resultCode === 0) {
+                    $io->success("'grunt less' has been successfully executed.");
+                } else {
+                    $io->error("'grunt less' failed. Please check the output for more details.");
+                    continue;
+                }
+                $isFirstRun = false;
+            } else {
+                $io->success("Grunt has been already executed.");
+            }
+
+            $io->section("Running 'php bin/magento setup:static-content:deploy -t $themeCode -f'... Please wait.");
+            exec("php bin/magento setup:static-content:deploy -t $themeCode -f", $outputLines, $resultCode);
+            $io->writeln($outputLines);
+            if ($resultCode === 0) {
+                $io->success("'php bin/magento setup:static-content:deploy -t $themeCode' has been successfully executed.");
+            } else {
+                $io->error("'php bin/magento setup:static-content:deploy -t $themeCode' failed. Please check the output for more details.");
+            }
+
+            $io->success("Theme $themeCode has been successfully built.");
         }
+
+        $endTime = microtime(true);
+        $duration = $endTime - $startTime;
+        $io->section("Build process completed.");
+        $io->success("All themes have been built successfully in " . round($duration, 2) . " seconds.");
+
         return Command::SUCCESS;
+    }
+
+    private function checkPackageJson(SymfonyStyle $io): bool
+    {
+        if (!file_exists('package.json')) {
+            $io->warning("The 'package.json' file does not exist in the Magento root path.");
+            if (!file_exists('package.json.sample')) {
+                $io->warning("The 'package.json.sample' file does not exist in the Magento root path.");
+                $io->error("Skipping this theme build.");
+                return false;
+            } else {
+                $io->success("The 'package.json.sample' file found.");
+                if ($io->confirm("Copy 'package.json.sample' to 'package.json'?", false)) {
+                    copy('package.json.sample', 'package.json');
+                    $io->success("'package.json.sample' has been copied to 'package.json'.");
+                }
+            }
+        } else {
+            $io->success("The 'package.json' file found.");
+        }
+        return true;
+    }
+
+    private function checkNodeModules(SymfonyStyle $io): bool
+    {
+        if (!is_dir('node_modules')) {
+            $io->warning("The 'node_modules' folder does not exist in the Magento root path.");
+            if ($io->confirm("Run 'npm install' to install the dependencies?", false)) {
+                $io->section("Running 'npm install'... Please wait.");
+                exec('npm install', $outputLines, $resultCode);
+                if ($resultCode === 0) {
+                    $io->success("'npm install' has been successfully executed.");
+                } else {
+                    $io->error("'npm install' failed. Please check the output for more details.");
+                    return false;
+                }
+            } else {
+                $io->error("Skipping this theme build.");
+                return false;
+            }
+        } else {
+            $io->success("The 'node_modules' folder found.");
+        }
+        return true;
+    }
+
+    private function checkFile(SymfonyStyle $io, string $file, string $sampleFile): bool
+    {
+        if (!file_exists($file)) {
+            $io->warning("The '$file' file does not exist in the Magento root path.");
+            if (!file_exists($sampleFile)) {
+                $io->warning("The '$sampleFile' file does not exist in the Magento root path.");
+                $io->error("Skipping this theme build.");
+                return false;
+            } else {
+                $io->success("The '$sampleFile' file found.");
+                if ($io->confirm("Copy '$sampleFile' to '$file'?", false)) {
+                    copy($sampleFile, $file);
+                    $io->success("'$sampleFile' has been copied to '$file'.");
+                }
+            }
+        } else {
+            $io->success("The '$file' file found.");
+        }
+        return true;
     }
 }
