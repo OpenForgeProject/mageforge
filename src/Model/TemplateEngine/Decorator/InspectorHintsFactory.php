3b0d5ae2ce818b19361c703d2e5c99264fe92a9f
Thomas Hauschild <7961978+Morgy93@users.noreply.github.com>
2026-01-29 23:32:57 +0100

    #84 - Add PHP config check and clear PHPStan cache in CI

--- src/Model/TemplateEngine/Decorator/InspectorHintsFactory.php
+++ src/Model/TemplateEngine/Decorator/InspectorHintsFactory.php
@@ -35,10 +35,13 @@ class InspectorHintsFactory
             );
         }
 
+        // Create instance - property access extracted to help PHPStan recognize the read
+        $randomGenerator = $this->random;
+        
         return new InspectorHints(
             $subject,
             (bool)$showBlockHints,
-            $this->random
+            $randomGenerator
         );
     }
 }
