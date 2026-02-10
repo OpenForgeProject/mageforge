0a952808b6a5f7102aa5ad4b874e576d67f77bd1
Mathias Elle <m.elle@dermatz.de>
2026-02-10 12:54:40 +0100

    feat: implement dark/light mode theme selection and admin configuration section (#131)

---
+++ src/Model/Config/Source/InspectorTheme.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenForgeProject\MageForge\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class InspectorTheme implements OptionSourceInterface
+{
+    /**
+     * @return array<int, array<string, string>>
+     */
+    public function toOptionArray(): array
+    {
+        return [
+            ['value' => 'dark', 'label' => (string) __('Dark')],
+            ['value' => 'light', 'label' => (string) __('Light')],
+            ['value' => 'auto', 'label' => (string) __('Auto (System Preference)')],
+        ];
+    }
+}
