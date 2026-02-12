c1fe894111e14387ecbc065024dd9efe18cd2393
Mathias Elle <m.elle@e3n.de>
2026-02-12 08:21:41 +0100

    feat: add method to retrieve theme by code

--- src/Model/ThemeList.php
+++ src/Model/ThemeList.php
@@ -27,4 +27,15 @@ class ThemeList
     {
         return $this->magentoThemeList->getItems();
     }
+
+    public function getThemeByCode(string $code): ?\Magento\Framework\View\Design\ThemeInterface
+    {
+        $themes = $this->getAllThemes();
+        foreach ($themes as $theme) {
+            if ($theme->getCode() === $code) {
+                return $theme;
+            }
+        }
+        return null;
+    }
 }
